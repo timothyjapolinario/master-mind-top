1ecafe66c5a005ab338baab63f11b8c6e1a552bd
JayJay <timothyjapolinario@gmail.com>
2022-05-10 11:53:59 +0800

    Guessing works for human

--- master-mind.rb
+++ master-mind.rb
@@ -5,6 +5,8 @@ class Game
         set_players(player_1_class, player_2_class)
         print @@master.secret_codes
         puts ""
+
+        play_game()
     end
     def set_players(player_1_class, player_2_class)
         puts "Input 1 to play as Master"
@@ -24,8 +26,15 @@ class Game
         end
     end
 
+    def play_game
+        while(@@master.secret_codes != @@cracker.guess_codes)
+            @@cracker.guess_secret_code
+        end
+    end
+    
+
+
     class Master
-        
         attr_reader :secret_codes
         def initialize(player_class)
             @secret_codes = []
@@ -33,17 +42,12 @@ class Game
         end
     
         def generate_secret_codes()
-            counter = 0
-            while(counter < 4)
-                secret_code = @@player.get_number()
-                @secret_codes.push(secret_code)
-                counter += 1
-            end
+            @secret_codes = @@player.get_number
         end
     end
     class Cracker
         @guess_codes = []
-    
+        attr_reader :guess_codes
         def initialize(player_class)
             @@player = player_class.new()
         end
@@ -52,6 +56,11 @@ class Game
             number = @@player.get_number
             return number
         end
+
+        def guess_secret_code
+            puts "Cracker is guessing the codes"
+            @guess_codes = @@player.get_number
+        end
     end
 end
 
@@ -59,15 +68,23 @@ end
 
 class Human
     def get_number
-        number = gets.chomp.to_i
-        return number
+        number = gets.chomp.split("")
+        if(number.length == 4)
+            return number
+        end
     end
 end
 
 class Computer
     def get_number
-        number = [1, 2, 3, 4, 5, 6].shuffle[0]
-        return number
+        counter = 0
+        all_number = ""
+        while(counter < 4)
+            number = ["1", "2", "3", "4", "5", "6"].shuffle[0]
+            all_number += number
+            counter += 1
+        end
+        return all_number.split("")
     end
 end
 
